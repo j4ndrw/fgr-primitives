513f8c6eb326fef9201fe02b75a55d2e8fe84355
j4ndrw <murjan_andreialin@yahoo.com>
2022-12-28 12:15:29 +0200

    chore: Added types

---
+++ packages/core/src/types/index.ts
@@ -0,0 +1,39 @@
+export interface Signal<T> {
+    get: () => T;
+    set: (value: T) => void;
+    update: (cb: (value: T) => T) => void;
+}
+
+export interface Runtime {
+    signalValues: any[];
+
+    effects: Effect[];
+    effectIdStack: (EffectId | null)[];
+
+    batches: Effect[];
+
+    subscriptionMap: Map<
+        SignalId,
+        {
+            delayedEffect: Effect | null;
+            effects: Set<EffectId>;
+        }
+    >;
+
+    createSignal: <T>(value: T) => Signal<T>;
+
+    createEffect: (effectFn: () => void) => void;
+    runEffect: (effectId: EffectId) => void;
+
+    batch: (cb: () => void) => void;
+    runBatch: (batchId: BatchId) => void;
+
+    unmount: (cleanupFn: () => void) => void;
+}
+
+export type SignalId = { value: number };
+
+export type Effect = () => void;
+export type EffectId = { value: number };
+
+export type BatchId = { value: number };
